4a5091095df83e93939ac03a25fa2d27cd213530
Shashini <ssshashini21@gmail.com>
2025-02-18 02:00:07 +0530

    Add nav bar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,36 @@
-
-
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div>
+      <nav className="flex justify-between items-center p-6 bg-gray-800 shadow-lg">
+        <h1 className="text-2xl font-bold">Shashini Aluthge</h1>
+        <ul className="flex space-x-6">
+          <li>
+            <a href="#about" className="hover:text-blue-400">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="hover:text-blue-400">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="hover:text-blue-400">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="hover:text-blue-400">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <button className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition">
+          Connect me
+        </button>
+      </nav>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
