4141495d7cd0b819e8d3ebeb20e5000eafe8d535
Shashini <ssshashini21@gmail.com>
2025-02-22 00:47:04 +0530

    make responsive home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,20 +43,21 @@ const Home: React.FC = () => {
   },[characterIndex,isDeleting,roleIndex,]);
 
   return (
-    <div className="bg-[#292929] min-h-screen ">
+    <div className="bg-[#292929] min-h-screen pt-16 lg:pt-0 ">
       <NavBar />
 
-      <div className="flex lg:flex-row sm:flex-col items-center h-[100vh] lg:ml-30 lg:mr-30 ">
-        <div className="flex-1 flex-col lg:justify-start sm:justify-center">
-          <h1 className="text-white font-bold text-5xl/20">Shashini Aluthge</h1>
-          <h2 className="text-white font-bold text-3xl">
+      <div className="flex flex-col lg:flex-row items-center justify-center lg:h-[100vh] lg:ml-30 lg:mr-30 pt-12 lg:pt-10 md:ml-10 md:mr-10 ml-2 mr-2">
+      <div className="box-content w-70 h-70 md:w-90 md:h-90 lg:w-100 lg:h-100 border-5 rounded-full lg:flex-1"></div>
+        <div className="flex-1 flex-col justify-center items-center pt-8 md:pt-2 lg:pt-0">
+          <h1 className="text-white font-bold text-4xl md:text-5xl/20">Shashini Aluthge</h1>
+          <h2 className="text-white font-bold text-xl md:text-2xl lg:text-3xl">
             I'm a {" "}
             <span className="text-[#00FF08]">{displayText}</span>
             <span className="text-[#00FF08] cursor">|</span>
           </h2>
-          <div className="mt-10 flex lg:flex-row sm:flex-col">
-            <div>
-              <button className="px-5 py-2 text-[#00FF08] rounded-4xl transition-all duration-200 bg-[#292929] shadow-[0_4px_7px_#00FF08] hover:shadow-[0_6px_8px_#00FF08] hover:scale-105 cursor-pointer">
+          <div className="mt-6  lg:mt-10 flex flex-col md:flex-row ">
+            <div className="text-center">
+              <button className="px-8 py-2 lg:px-5 text-[#00FF08] rounded-4xl transition-all duration-200 bg-[#292929] shadow-[0_4px_7px_#00FF08] hover:shadow-[0_6px_8px_#00FF08] hover:scale-105 cursor-pointer">
                 Download CV
               </button>
             </div>
@@ -64,7 +65,7 @@ const Home: React.FC = () => {
             <SocialIcons />
           </div>
         </div>
-        <div className="box-content w-100 h-100 border-5 rounded-full lg:justify-right"></div>
+    
       </div>
     </div>
   );
