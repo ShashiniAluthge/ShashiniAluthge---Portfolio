de3949fce71413941eafc63d85644adff1555a9c
Shashini <ssshashini21@gmail.com>
2025-02-21 22:11:40 +0530

    Add role text animation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
+import { Menu } from "lucide-react";
+import Drawer from "./Drawer";
 
 const NavBar = () => {
   const [active, setActive] = useState("home");
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
-    <div className="fixed top-0 right-30 left-30 bg-[#292929]">
+    <div className="fixed top-0 lg:right-30 lg:left-30 bg-[#292929] sm:left-0 sm:right-0">
       <nav className="flex justify-between items-center p-6">
         <h1 className="text-2xl font-bold text-white tracking-wide uppercase">
           <span className="text-[#00FF08] italic font-serif text-3xl">S</span>
           hashini
         </h1>
-        <ul className="flex space-x-6">
+
+        <ul className="hidden lg:flex space-x-6">
           <li
             onClick={() => setActive("home")}
             className={`px-4 py-1 rounded-4xl cursor-pointer transition ${
               active === "home"
                 ? "text-[#00FF08] font-bold"
-                : "text-[#fff] hover:text-[#00FF08] hover:font-bold"
+                : "text-[#fff] hover:text-[#00FF08] hover:font-bold "
             }`}
           >
             <a href="#home">Home</a>
@@ -66,10 +70,18 @@ const NavBar = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
-        <button className="px-5 py-2 text-[#00FF08] rounded-4xl transition-all duration-200 bg-[#292929] shadow-[0_4px_7px_#00FF08] hover:shadow-[0_6px_8px_#00FF08] hover:scale-105 cursor-pointer">
+        <button className="hidden lg:block px-5 py-2 text-[#00FF08] rounded-4xl transition-all duration-200 bg-[#292929] shadow-[0_4px_7px_#00FF08] hover:shadow-[0_6px_8px_#00FF08] hover:scale-105 cursor-pointer">
           Connect me
         </button>
+
+        <button
+          className="lg:hidden text-white"
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          <Menu size={28} color="#fff" />
+        </button>
       </nav>
+      <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import SocialIcons from "../components/SocialIcons";
 
+const roles =["Full Stack Developer", "Programmer","UI/UX Designer"];
+
 const Home: React.FC = () => {
+
+  const [roleIndex,setRoleIndex] = useState(0);
+  const [displayText,setDisplayText] = useState("");
+  const [characterIndex,setCharacterIndex] = useState(0);
+  const [isDeleting,setIsDeleting] = useState(false);
+
+  useEffect(()=>{
+    const currentRole = roles[roleIndex];
+//Add letters one by one
+    if(!isDeleting && characterIndex <currentRole.length){
+      setTimeout(() => {
+        setDisplayText((prev) => prev + currentRole[characterIndex]);
+        setCharacterIndex((prev) => prev + 1);
+      }, 100);
+    }
+    else if (isDeleting && characterIndex > 0) {
+      // Remove letters one by one
+      setTimeout(() => {
+        setDisplayText((prev) => prev.slice(0, -1));
+        setCharacterIndex((prev) => prev - 1);
+      }, 50);
+  }
+  else {
+    // Pause before switching roles
+    setTimeout(() => {
+      if (!isDeleting) {
+        setIsDeleting(true);
+      } else {
+        setIsDeleting(false);
+        setRoleIndex((prev) => (prev + 1) % roles.length);
+        setCharacterIndex(0);
+        setDisplayText("");
+      }
+    }, 1000);
+  }
+  },[characterIndex,isDeleting,roleIndex,]);
+
   return (
     <div className="bg-[#292929] min-h-screen ">
-      
       <NavBar />
 
-      <div className="flex flex-row  items-center h-[100vh] ml-30 mr-30">
-        <div className="flex-1 flex-col justify-start ">
+      <div className="flex lg:flex-row sm:flex-col items-center h-[100vh] lg:ml-30 lg:mr-30 ">
+        <div className="flex-1 flex-col lg:justify-start sm:justify-center">
           <h1 className="text-white font-bold text-5xl/20">Shashini Aluthge</h1>
           <h2 className="text-white font-bold text-3xl">
-            I'm a<span className="text-[#00FF08]"> Full Stack Developer</span>
+            I'm a {" "}
+            <span className="text-[#00FF08]">{displayText}</span>
+            <span className="text-[#00FF08] cursor">|</span>
           </h2>
-          <div className="mt-10 flex flex-row">
+          <div className="mt-10 flex lg:flex-row sm:flex-col">
             <div>
               <button className="px-5 py-2 text-[#00FF08] rounded-4xl transition-all duration-200 bg-[#292929] shadow-[0_4px_7px_#00FF08] hover:shadow-[0_6px_8px_#00FF08] hover:scale-105 cursor-pointer">
                 Download CV
@@ -24,7 +64,7 @@ const Home: React.FC = () => {
             <SocialIcons />
           </div>
         </div>
-        <div className="box-content size-100 border-5 rounded-full p-5 justify-right "></div>
+        <div className="box-content w-100 h-100 border-5 rounded-full lg:justify-right"></div>
       </div>
     </div>
   );
