ac9e5f6fa16341f58f15819b12709d58fc92dd07
Shashini <ssshashini21@gmail.com>
2025-02-25 04:37:23 +0530

    complete projects section

--- src/components/MobileAppDevelopment.tsx
+++ src/components/MobileAppDevelopment.tsx
@@ -1,47 +1,57 @@
-import webIcon from "../assets/webIcon.png";
-import GameWebImage from "../assets/GameWeb.png";
 import GitIcon from "../assets/github.png";
-import GymClientImage from "../assets/GymClient.png";
-import PortfolioImage from "../assets/Portfolio.png";
+import Xpress from "../assets/Xpress.png";
+import mobileIcon from "../assets/mobileIcon.png";
+import careAlertImage from "../assets/careAlert.png";
 
-const WebAppDevelopment = () => {
+const MobileAppDevelopment = () => {
   return (
     <>
       <div>
         <h1 className="text-center text-[var(--secondary)] md:text-3xl lg:text-3xl mt-8 text-2xl mb-5 font-bold flex justify-center">
           <img
-            src={webIcon}
+            src={mobileIcon}
             className="w-8 h-8 md:w-10 md:h-10 mr-2 md:mr-10 "
           />
-          Web App Development
+          Mobile App Development
         </h1>
-        {/* for Game Hub */}
+        {/* for Xpress */}
         <div className="w-full flex flex-col lg:flex-row container mx-auto px-2 py-5">
           {/* for image */}
           <div className="flex flex-1 p-2 items-center justify-center">
             <img
-              src={GameWebImage}
+              src={Xpress}
               className="w-[80%] h-[80%] md:w-[60%]  lg:rounded-2xl rounded-lg"
             />
           </div>
           <div className="flex-1 p-4 ">
             {/* for project name */}
             <p className="text-[var(--primary)] font-semibold  text-2xl">
-              Gaming Website
+              XPress - Courier Service Management System
             </p>
             {/* for project description */}
             <p className="text-[var(--secondary)] lg:text-lg text-md lg:mt-2 mt-3 font-light text-justify">
-              I developed a dynamic Game Hub website using React, Vite, and
-              TypeScript, incorporating tools like React Query, Zustand, and
-              Chakra UI for enhanced functionality and design.
+              ( Second Year Software Project )<br />
+              This system was designed to streamline personal courier services
+              while addressing communication failures and the lack of trust in
+              external providers. It operates through both web and mobile
+              applications, ensuring secure and efficient document and package
+              transfers.
+              <br />I played a key role in developing the full stack mobile
+              application development.
             </p>
             {/*for Technologies */}
             <div className="grid grid-cols-2 md:flex md:flex-row  gap-3 mt-2">
               <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                React.js
+                React Native
               </p>
               <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                TypeScript
+                Node.js
+              </p>
+              <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
+                Express.js
+              </p>
+              <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
+                MySQL
               </p>
             </div>
             {/* divider */}
@@ -50,7 +60,7 @@ const WebAppDevelopment = () => {
             {/* for github link */}
             <div className=" border-2 rounded-full w-10 p-2 border-[var(--secondbackground)] button_shadow hover:button_shadow-hover hover:scale-105 cursor-pointer">
               <a
-                href="https://github.com/ShashiniAluthge/game-hub"
+                href="https://github.com/Courier-Service-Project"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex justify-left"
@@ -61,34 +71,33 @@ const WebAppDevelopment = () => {
           </div>
         </div>
 
-        {/* for Gym client web*/}
+        {/* for care alert*/}
         <div className="w-full flex flex-col lg:flex-row-reverse container mx-auto px-2 py-5">
           {/* for image */}
-          <div className="flex flex-1 p-2 items-center justify-center">
+          <div className="flex flex-1 p-2 items-center justify-center rounded-4xl ">
             <img
-              src={GymClientImage}
-              className="w-[80%] h-[80%] md:w-[60%] lg:rounded-2xl rounded-lg"
+              src={careAlertImage}
+              className="w-[50%] md:w-[40%] lg:rounded-4xl rounded-lg max-h-[320px]  "
             />
           </div>
           <div className="flex-1 p-4 ">
             {/* for project name */}
             <p className="text-[var(--primary)] font-semibold  text-2xl">
-              Gym Management System Client Website
+              Care Alert Mobile App
             </p>
             {/* for project description */}
             <p className="text-[var(--secondary)] lg:text-lg text-md lg:mt-2 mt-3 font-light text-justify">
-              I developed the client-side web application for a gym management
-              system, enabling users to manage workouts, track progress, view
-              payments, and explore gym details, membership plans, and contact
-              information.
+              I developed Care Alert, a mobile app that fetches global COVID-19
+              updates via a public API. It includes registration, login, home,
+              and detail views, with a focus on front-end development.
             </p>
             {/*for Technologies */}
             <div className="grid grid-cols-2 md:flex md:flex-row  gap-3 mt-2">
               <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                React.js
+                React Native
               </p>
               <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                TypeScript
+                JavaScript
               </p>
             </div>
             {/* divider */}
@@ -97,56 +106,7 @@ const WebAppDevelopment = () => {
             {/* for github link */}
             <div className=" border-2 rounded-full w-10 p-2 border-[var(--secondbackground)] button_shadow hover:button_shadow-hover hover:scale-105 cursor-pointer">
               <a
-                href="https://github.com/GymManagmentSystem/gym_client_web_app"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex justify-left"
-              >
-                <img src={GitIcon} className="w-full h-full" />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        {/* for portfolio  */}
-        <div className="w-full flex flex-col lg:flex-row container mx-auto px-2 py-5">
-          {/* for image */}
-          <div className="flex flex-1 p-2 items-center justify-center">
-            <img
-              src={PortfolioImage}
-              className="w-[80%] h-[80%] md:w-[60%] lg:rounded-2xl rounded-lg"
-            />
-          </div>
-          <div className="flex-1 p-4 ">
-            {/* for project name */}
-            <p className="text-[var(--primary)] font-semibold  text-2xl">
-              My Personal Portfolio
-            </p>
-            {/* for project description */}
-            <p className="text-[var(--secondary)] lg:text-lg text-md lg:mt-2 mt-3 font-light text-justify">
-              I developed my personal portfolio website using React, TypeScript,
-              and Tailwind CSS, showcasing my projects, skills, and experience
-              with a clean, responsive design.
-            </p>
-            {/*for Technologies */}
-            <div className="grid grid-cols-2 md:flex md:flex-row  gap-3 mt-2">
-              <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                React.js
-              </p>
-              <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                TypeScript
-              </p>
-              <p className="inline-flex justify-center items-center text-[var(--primary)]  text-md font-semibold border-2 rounded-lg p-2 lg:px-4 py-1 w-auto">
-                Tailwind CSS
-              </p>
-            </div>
-            {/* divider */}
-            <hr className="border-2 border-[var(--secondbackground)] mt-5 mb-2" />
-
-            {/* for github link */}
-            <div className=" border-2 rounded-full w-10 p-2 border-[var(--secondbackground)] button_shadow hover:button_shadow-hover hover:scale-105 cursor-pointer">
-              <a
-                href="https://github.com/ShashiniAluthge/ShashiniAluthge---Portfolio"
+                href="https://github.com/ShashiniAluthge/CareAlertMobileApp"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex justify-left"
@@ -161,4 +121,4 @@ const WebAppDevelopment = () => {
   );
 };
 
-export default WebAppDevelopment;
+export default MobileAppDevelopment;
