78f7f57ea21fd0e88605e21e9e502989756e9e7d
Shashini Aluthge <ssshashini21@gmail.com>
2025-12-31 11:32:13 +0530

    Update cv

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -90,7 +90,7 @@ const Home = () => {
             <div className="text-center">
               <button className="px-8 py-2 lg:px-5 text-[var(--primary)] rounded-4xl transition-all duration-200 bg-[var(--background)] button_shadow hover:button_shadow-hover hover:scale-105 cursor-pointer">
                 <a
-                  href="https://drive.google.com/file/d/1doqfoJv6j-RauxuK7RE6TGC_PjLqbdj8/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1Yyjt60AzCpOqku4hCYm85D09fT_rsA29/view?usp=sharing"
                   download="Shashini_Aluthge_CV.pdf"
                   target="_blank"
                 >
